7c1ceda7a578399985834fc7ec1eade5a2e713fc
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-06-20 18:30:24 -0400

    fix main gitignore path

--- src/git.config.sh
+++ src/git.config.sh
@@ -1,7 +1,7 @@
 # configuration goes to ~/.gitconfig
 git config --global core.editor "vim"
 git config --global pager.branch false
-git config --global --replace-all core.excludesfile ~/.after-install/main.gitignore
+git config --global --replace-all core.excludesfile ~/.after-install/src/main.gitignore
 
 # Colors
 git config --global color.diff.meta cyan
