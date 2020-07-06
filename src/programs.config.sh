3dbd0fecce37c964bb508a57dcc25ce5da797a14
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-07-06 15:27:40 -0400

    modify git diffs to show highlighted words

--- src/programs.config.sh
+++ src/programs.config.sh
@@ -1,9 +1,16 @@
 ## git configuration
 if git --version >/dev/null 2>&1; then
   # configuration goes to ~/.gitconfig
+
   git config --global core.editor "vim"
-  git config --global pager.branch false
+  git config --global pager.branch false # print branch info instead of "less" it
   git config --global --replace-all core.excludesfile ~/.after-install/src/main.gitignore
+
+  # Highlight changed words in git diff (dpkg -L git | grep diff-highlight)
+  git config --global pager.log "perl /usr/share/doc/git/contrib/diff-highlight/diff-highlight | less"
+  git config --global pager.show "perl /usr/share/doc/git/contrib/diff-highlight/diff-highlight | less"
+  git config --global pager.diff "perl /usr/share/doc/git/contrib/diff-highlight/diff-highlight | less"
+  git config --global interactive.diffFilter "perl /usr/share/doc/git/contrib/diff-highlight/diff-highlight"
 
   # Colors
   git config --global color.diff.meta cyan
