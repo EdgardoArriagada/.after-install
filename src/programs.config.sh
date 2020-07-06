5166a92225aeec056dde026f7f0f0d4bd9c85e7a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-07-06 19:40:08 -0400

    modify git diff colors

--- src/programs.config.sh
+++ src/programs.config.sh
@@ -13,8 +13,19 @@ if git --version >/dev/null 2>&1; then
   git config --global interactive.diffFilter "perl /usr/share/doc/git/contrib/diff-highlight/diff-highlight"
 
   # Colors
-  git config --global color.diff.meta cyan
-  git config --global color.diff.commit magenta
+  git config --global color.ui true
+
+  git config --global color.diff-highlight.oldNormal "red bold"
+  git config --global color.diff-highlight.oldHighlight "red bold 52"
+  git config --global color.diff-highlight.newNormal "green bold"
+  git config --global color.diff-highlight.newHighlight "green bold 22"
+
+  git config --global color.diff.meta "11"
+  git config --global color.diff.frag "magenta bold"
+  git config --global color.diff.commit "yellow bold"
+  git config --global color.diff.old "red bold"
+  git config --global color.diff.new "green bold"
+  git config --global color.diff.whitespace "red reverse"
 fi
 
 ## zsh configuration
