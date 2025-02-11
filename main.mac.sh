a691b3a204d52022ead579dc479557a02726c05a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-02-10 21:29:56 -0300

    refactor into an array

--- main.mac.sh
+++ main.mac.sh
@@ -1,37 +1,46 @@
-brew install --cask ghostty font-hack-nerd-font flameshot postman
+cask_apps=(
+  ghostty
+  font-hack-nerd-font
+  flameshot
+  postman
+)
 
-brew install \
-  asdf \
-  tmuxp \
-  docker \
-  docker-compose \
-  colima \
-  stow \
-  prettierd \
-  gopls \
-  stylua \
-  lsd \
-  tmux \
-  hammerspoon \
-  bat \
-  tig \
-  neovim \
-  ripgrep \
-  sd \
-  nvm \
-  gnupg2 \
-  gh \
-  pinentry-mac \
-  fd \
-  tldr \
-  git-delta \
-  sox \
-  coreutils \
-  fzf \
-  btop \
-  font-caskaydia-cove-nerd-font \
-  koekeishiya/formulae/skhd \
+brew_apps=(
+  asdf
+  tmuxp
+  docker
+  docker-compose
+  colima
+  stow
+  prettierd
+  gopls
+  stylua
+  lsd
+  tmux
+  hammerspoon
+  bat
+  tig
+  neovim
+  ripgrep
+  sd
+  nvm
+  gnupg2
+  gh
+  pinentry-mac
+  fd
+  tldr
+  git-delta
+  sox
+  coreutils
+  fzf
+  btop
+  font-caskaydia-cove-nerd-font
+  koekeishiya/formulae/skhd
   choose-gui
+)
+
+brew install --cask "${cask_apps[@]}"
+brew install "${brew_apps[@]}"
 
 skhd --start-service
 
